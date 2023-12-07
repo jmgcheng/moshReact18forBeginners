2bb213d207be49fd915602b9c6d2f1fa1c6a4c6c
jmgcheng <jmgcheng@users.noreply.github.com>
2023-12-08 07:55:31 +0800

    04-buildingComponents-conditionalRendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,14 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
   return (
     <>
-      <h1>List Group</h1>
+      <h1>List</h1>
+      {items.length === 0 && <p>No Items Found.</p>}
+      {/*
+        - short circuit
+        - items.length === 0 should be false to continue executing below
+      */}
       <ul className="list-group">
         {items.map((item) => (
           <li className="list-group-item" key={item}>
