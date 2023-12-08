1ef709431d296f61cd170cfc6d9c31edaba32fb0
jmgcheng <jmgcheng@users.noreply.github.com>
2023-12-08 08:09:02 +0800

    05-buildingComponents-handlingEvents

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,9 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
@@ -10,8 +14,8 @@ function ListGroup() {
         - items.length === 0 should be false to continue executing below
       */}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item}>
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
             {item}
           </li>
         ))}
