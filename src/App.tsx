d5e385f12d519bd3db59a3beb1e639d3a1c639a1
jmgcheng <jmgcheng@users.noreply.github.com>
2023-12-08 12:50:35 +0800

    12-buildingComponents-showingAnAlert

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Button color="warning" onClick={() => console.log("click")}>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button color="warning" onClick={() => setAlertVisibility(true)}>
         My Button
       </Button>
     </div>
