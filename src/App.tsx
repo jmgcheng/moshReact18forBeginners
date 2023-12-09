e3d8e566e7823f85c6a250e5fad8a48f8ba98a7c
jmgcheng <jmgcheng@users.noreply.github.com>
2023-12-09 12:07:28 +0800

    22-managingComponentState-buildingExpandableTextComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import { useState } from "react";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      { id: 1, title: "Product 1", quantity: 1 },
-      { id: 2, title: "Product 2", quantity: 1 },
-    ],
-  });
-
-  const handleClick = () => {
-    setCart({
-      ...cart,
-      items: cart.items.map((item) =>
-        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
-      ),
-    });
-  };
-
   return (
     <div>
-      <button onClick={handleClick}>Click Me</button>
+      <ExpandableText maxChars={10}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus
+        dignissimos hic officia nulla nesciunt deserunt architecto tempora
+        delectus, enim atque rem. Fugiat delectus nesciunt magnam temporibus,
+        repellat unde ipsa eos! Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Voluptatibus dignissimos hic officia nulla nesciunt
+        deserunt architecto tempora delectus, enim atque rem. Fugiat delectus
+        nesciunt magnam temporibus, repellat unde ipsa eos! Lorem ipsum dolor
+        sit, amet consectetur adipisicing elit. Voluptatibus dignissimos hic
+        officia nulla nesciunt deserunt architecto tempora delectus, enim atque
+        rem. Fugiat delectus nesciunt magnam temporibus, repellat unde ipsa eos!
+      </ExpandableText>
     </div>
   );
 }
