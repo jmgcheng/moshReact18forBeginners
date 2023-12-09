cf71b565d4539ab62bc38a8981b5e13d2f85e244
jmgcheng <jmgcheng@users.noreply.github.com>
2023-12-09 11:44:51 +0800

    import { useState } from react;

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
 
 function App() {
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: "John",
-    },
+  const [pizza, setPizza] = useState({
+    name: "Spicy Pepperoni",
+    toppings: ["Mushroom"],
   });
 
   const handleClick = () => {
-    setGame({ ...game, player: { ...game.player, name: "Bob" } });
+    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
   };
 
-  return <div></div>;
+  return (
+    <div>
+      <button onClick={handleClick}>Click Me</button>
+    </div>
+  );
 }
 
 export default App;
