3e2b846debe4212954c4c564616e0637c188e95a
jmgcheng <jmgcheng@users.noreply.github.com>
2023-12-10 09:33:35 +0800

    23-buildingForms-buildingAForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import { useState } from "react";
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText maxChars={10}>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus
-        dignissimos hic officia nulla nesciunt deserunt architecto tempora
-        delectus, enim atque rem. Fugiat delectus nesciunt magnam temporibus,
-        repellat unde ipsa eos! Lorem ipsum dolor sit, amet consectetur
-        adipisicing elit. Voluptatibus dignissimos hic officia nulla nesciunt
-        deserunt architecto tempora delectus, enim atque rem. Fugiat delectus
-        nesciunt magnam temporibus, repellat unde ipsa eos! Lorem ipsum dolor
-        sit, amet consectetur adipisicing elit. Voluptatibus dignissimos hic
-        officia nulla nesciunt deserunt architecto tempora delectus, enim atque
-        rem. Fugiat delectus nesciunt magnam temporibus, repellat unde ipsa eos!
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
